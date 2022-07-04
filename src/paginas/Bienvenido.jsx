9445962bb2758e84889c012cf661345af75404b2
efrainloera <efrainloerub578@gmail.com>
2022-07-03 23:08:07 -0500

    feat: added fixes with some missing features

--- src/paginas/Bienvenido.jsx
+++ src/paginas/Bienvenido.jsx
@@ -15,32 +15,15 @@ export const Bienvenido = () => {
         getData()
     }, [])
 
-
-    const pokemonAleatorio = () => {
-        const numero = Math.floor(Math.random() * count)
-        navigate(`/pokemon/${numero}`);
-    }
-
     return (
         <div className=' items-center justify-center'>
 
             <div className=" text-center  ">
                 <h5 className="mb-2 bg-yellow-400 text-3xl font-bold  ">
                     Bienvenidos, pagina sobre pokemones :3
-                </h5>
-                
-                
-                    <button onClick={() => pokemonAleatorio()} className="w-full bg-yellow-400  text-red-600 rounded-lg inline-flex items-center justify-center px-4 py-2.5">
-                        <div className="text-left">
-                            <div className="-mt-1 font-sans text-sm font-semibold">Caracteristicas de Pokemones al Azar</div>
-                        </div>
-                    </button>
-                  
+                </h5>    
             </div>
-
-
-        </div>
-
+ </div>
     )
 }
 
